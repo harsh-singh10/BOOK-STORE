f94cbbabe21e98884f54ca96c82c9371edafda61
harsh-singh10 <harshgbkumar0139@gmail.com>
2024-08-09 23:49:00 +0530

    update url

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -15,8 +15,8 @@ function Login() {
       email: data.email,
       password: data.password,
     };
-    await axios
-      .post("http://localhost:4001/user/login", userInfo)
+    
+    await axios.post("https://book-store-backend-mwp3.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
