f58330f84b55701ca4842e54ddb857750fd593f6
Harsh Kumar <121729068+harsh-singh10@users.noreply.github.com>
2024-08-10 10:21:25 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import Home from "./home/Home";
-import { Navigate, Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider";
 
-function App() {
-  const [authUser, setAuthUser] = useAuth();
-  console.log(authUser);
+const App = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <>
-      <div className="dark:bg-slate-900 dark:text-white">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/course"
-            element={authUser ? <Courses /> : <Navigate to="/signup" />}
-          />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-        <Toaster />
-      </div>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route 
+          path="/courses" 
+          element={isAuthenticated ? <Courses /> : <Navigate to="/signup" />} 
+        />
+        <Route path="/signup" element={<Signup />} />
+        {/* Add other routes here as needed */}
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
