9bb0f0140dfa7747b997bf40c146bfe3f0d9e7ed
Harsh Kumar <121729068+harsh-singh10@users.noreply.github.com>
2024-08-10 09:51:55 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,33 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
+ // app.use(cors());
+
+// new 
+// Configure CORS
+const allowedOrigins = ['https://book-store-frontend-d3fu.onrender.com'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        // Allow request from the allowed origins
+        callback(null, true);
+      } else {
+        // Reject request from not allowed origins
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+  })
+);
+
+
+// new
+
+
+
 app.use(express.json());
 
 dotenv.config();
