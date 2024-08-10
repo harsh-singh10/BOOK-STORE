cb34e3c67189edada8bbc26a36e370b5f3c93b24
Harsh Kumar <121729068+harsh-singh10@users.noreply.github.com>
2024-08-10 09:04:03 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-    await axios.post("http://localhost:4001/user/signup", userInfo)
+    await axios.post("https://book-store-backend-icuk.onrender.com/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
